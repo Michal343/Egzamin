886d8aa0e4aaa3e137a050cf9b3809701f9ccca8
Michał Waczyński <michael.waczynski@gmail.com>
2026-05-14 23:09:46 +0200

    tescik

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,6 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   darkMode: 'class',
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  base: "/egzamin/",
+  base: "/Egzamin/",
   plugins: [react(),tailwindcss(),],
 })
